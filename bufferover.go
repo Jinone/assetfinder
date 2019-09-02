32fbcfc2579782aacf7e3fbaa05777a51db60937
Jinone <44525519+Jinone@users.noreply.github.com>
2019-09-02 23:09:22 +0800

    Create bufferover.go
    just study

---
+++ bufferover.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"strings"
+)
+
+type Bufferover struct {
+	Meta struct {
+		Runtime string `json:"Runtime"`
+		Errors interface{} `json:"Errors"`
+		Message string `json:"Message"`
+		FileNames []string `json:"FileNames"`
+		TOS string `json:"TOS"`
+	} `json:"Meta"`
+	FDNSA []string `json:"FDNS_A"`
+	RDNS []string `json:"RDNS"`
+}
+func fetchBufferover(domain string) ([]string,error) {
+	var s []string
+	fetchURL := fmt.Sprintf("https://dns.bufferover.run/dns?q=.%s", domain)
+	resp, err := http.Get(fetchURL)
+	if err != nil {
+		panic(err)
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	b := Bufferover{}
+	json.Unmarshal([]byte(data), &b)
+	for _,c := range b.FDNSA{
+		d := strings.Split(c,",")
+		s = append(s, d[1])
+	}
+	for _,j := range b.RDNS{
+		h := strings.Split(j,",")
+		s = append(s, h[1])
+	}
+   return s,nil
+}
+
+
+
