b6c658d306dedacf43d2a137fb3031d3c1c0a942
Jinone <44525519+Jinone@users.noreply.github.com>
2019-09-03 10:18:33 +0800

    Create passivetotal.go

---
+++ passivetotal.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+"encoding/json"
+"fmt"
+"io/ioutil"
+"net/http"
+"os"
+)
+
+type Jsons struct {
+	Subdomains []string `json:"subdomains"`
+	Success bool `json:"success"`
+	QueryValue string `json:"queryValue"`
+	PrimaryDomain string `json:"primaryDomain"`
+}
+
+func fetchPassivetotal(domain string) ([]string, error) {
+	apiEmail := os.Getenv("RISK_EMAIL")
+	apiKey := os.Getenv("RISK_KEY")
+	if apiKey == "" && apiEmail == ""{
+		return []string{}, nil
+	}
+	client := &http.Client{}
+	fetchURL := fmt.Sprintf("https://api.passivetotal.org/v2/enrichment/subdomains?query=%s", domain)
+	req, err := http.NewRequest("GET", fetchURL, nil)
+	req.SetBasicAuth(apiEmail, apiKey)
+	if err != nil {
+		panic(err)
+	}
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	s := Jsons{}
+	var j []string
+	json.Unmarshal([]byte(data), &s)
+	fmt.Println(s.Subdomains)
+	for _,c := range s.Subdomains{
+		d := c + "." + domain
+		j = append(j,d)
+	}
+	return j, err
+}
